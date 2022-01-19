f57ce6c00df4f631c5f59737ed343df1870bc96c
QSP <QSP@QSP>
2022-01-19 07:15:14 +0530

    Arrays

---
+++ src/arrays/Test1.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+  String s[]=new String[4];
+  s[0]="Ramesh";
+  s[1]="Suresh";
+  s[2]="Ganesh";
+  s[3]="Mahesh";
+ 
+  
+  for(int i=0;i<=3;i++) {
+	  System.out.println(s[i]);
+  }
+
+
+	}
+
+}
---
+++ src/arrays/Test3.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+char c[]=new char[4];
+c[0]='A';
+c[2]='B';
+c[1]='C';
+c[3]='D';
+
+for(int i=0;i<=3;i++) {
+	System.out.println(c[i]);
+}
+
+	}
+
+}
